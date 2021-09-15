d01e941476a9a74a268635abc988c8731bba600d
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-09-15 13:41:51 +0200

    Started a new try for the tech demo since it was 2 months since I worked on the previous project.
    I don't remember how and why i did some things so to learn more I'm starting over :)

---
+++ Tech_Demo_2.0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ground =
+        if (!isGrounded)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            controller.Move(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            controller.Move(new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime));
+        }
+    }
+}
