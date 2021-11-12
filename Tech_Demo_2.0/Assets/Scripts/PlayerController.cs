8fc5ce38bb0150fa45243adfcc4ebd3c09dd6ff4
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-11-12 13:00:13 +0100

    built prototype area and worked on the movement.

--- Tech_Demo_2.0/Assets/Scripts/PlayerController.cs
+++ Tech_Demo_2.0/Assets/Scripts/PlayerController.cs
@@ -5,31 +5,82 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController controller;
-    bool isGrounded;
+    private bool isGrounded;
+
+    private float verticalInput;
+    private float horizontalInput;
+
+    [SerializeField] float speed;
+    [SerializeField] float quickSpeed;
+
+    private float currentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        currentSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Ray ground =
+        if (Input.GetKey(KeyCode.LeftShift) && currentSpeed != quickSpeed)
+        {
+            currentSpeed = quickSpeed;
+        }
+        else
+        {
+            if (currentSpeed != speed)
+            {
+                currentSpeed = speed;
+            }
+        }
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, Mathf.Infinity);
+        Debug.DrawRay(transform.position, Vector3.down * 10, Color.red, 2);
+        Debug.DrawLine(transform.position, hit.point, Color.blue);
+
+        // Make sure the player is on the ground.
         if (!isGrounded)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x, hit.point.y + transform.lossyScale.y, transform.position.z);
         }
 
-        if (Input.GetAxis("Horizontal") != 0)
+        if (horizontalInput != 0)
         {
-            controller.Move(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Time.deltaTime);
+            controller.Move(new Vector3(horizontalInput, 0, 0) * Time.deltaTime * currentSpeed);
+            transform.position = new Vector3(transform.position.x, hit.point.y + transform.lossyScale.y, transform.position.z);
         }
 
-        if (Input.GetAxis("Vertical") != 0)
+        if (verticalInput != 0)
         {
-            controller.Move(new Vector3(0, 0, Input.GetAxis("Vertical") * Time.deltaTime));
+            controller.Move(new Vector3(0, 0, verticalInput) * Time.deltaTime * currentSpeed);
+            transform.position = new Vector3(transform.position.x, hit.point.y + transform.lossyScale.y, transform.position.z);
+        }
+
+        Debug.Log(isGrounded);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            Debug.Log("enter");
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            Debug.Log("exit");
+            isGrounded = false;
         }
     }
 }
