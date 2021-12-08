ee6fca749a9b10665b73a92e77786b132a370843
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-12-08 23:40:52 +0100

    Second area terrain and portal prefab.

---
+++ Tech_Demo_2.0/Assets/Scripts/Area.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour
+{
+    // TODO make into propertys?? Need to be a monobehaviour? to attach to game object??
+    public string Name;
+    public bool IsExplored;
+}
