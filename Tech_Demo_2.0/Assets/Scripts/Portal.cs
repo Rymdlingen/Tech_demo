f39ba9d6b5f7d2f231580b373a80df1ebf7479e6
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2021-12-09 15:12:18 +0100

    Portals can move player between them, but not in a good way.

--- Tech_Demo_2.0/Assets/Scripts/Portal.cs
+++ Tech_Demo_2.0/Assets/Scripts/Portal.cs
@@ -4,15 +4,107 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Properties.
+    public bool isActivated
     {
-        
+        get; private set;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Private fields.
+    [SerializeField] private Portal destination;
+    private Camera portalCamera;
+    private MeshRenderer screenMeshRenderer;
+    private RenderTexture screenRenderTexture;
+
+    [SerializeField] private GameObject player;
+    private Camera playerCamera;
+
+    // Can it be string for a name of the portal traveler or can I just use PortalTraveler? Or will that be the same for different travelers??
+    private Dictionary<PortalTraveler, PortalSide> travelers;
+
+    // Enums.
+    enum PortalSide
     {
-        
+        Posetive,
+        Negative
+    }
+
+
+    void Start()
+    {
+
+        // TODO player = GameObject.FindGameObjectWithTag("Player");
+        travelers = new Dictionary<PortalTraveler, PortalSide> { };
+    }
+
+    void LateUpdate()
+    {
+        foreach (KeyValuePair<PortalTraveler, PortalSide> traveler in travelers)
+        {
+            // TODO change to != when the calculation method is actually calculating.
+            if (traveler.Value == CalculatePortalSide(traveler.Key))
+            {
+                player.GetComponent<PortalTraveler>().Travel(destination);
+                RemoveTraveler(traveler.Key);
+            }
+        }
+    }
+
+    private void MoveCamera()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+
+        if (other.GetComponent<PortalTraveler>() != null)
+        {
+            AddTraveler(other.GetComponent<PortalTraveler>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+
+        if (travelers.ContainsKey(other.GetComponent<PortalTraveler>()))
+        {
+            // RemoveTraveler(other.GetComponent<PortalTraveler>());
+        }
+    }
+
+    private void AddTraveler(PortalTraveler traveler)
+    {
+        travelers.Add(traveler, CalculatePortalSide(traveler));
+
+        Debug.Log(travelers);
+    }
+
+    private void RemoveTraveler(PortalTraveler traveler)
+    {
+        travelers.Remove(traveler);
+    }
+
+    private void TrackTravelers()
+    {
+
+    }
+
+    private void MoveTraveler(PortalTraveler traveler)
+    {
+
+    }
+
+    private PortalSide CalculatePortalSide(PortalTraveler traveler)
+    {
+        // TODO
+        return PortalSide.Posetive;
+    }
+
+    private void SetScreenRenderTexture()
+    {
+
     }
 }
